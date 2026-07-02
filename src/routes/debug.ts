6ccfcc60ca1bafd67862c6d86574cf31eef5c3a4
hugolamcloser <hugolamcloser@gmail.com>
2026-07-03 00:07:11 +0800

    Fix LINE inbound message forwarding diagnostics

--- src/routes/debug.ts
+++ src/routes/debug.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { getEnvPresenceReport } from "../config/env";
+import { getRecentDebugEvents } from "../services/repository";
 
 export const debugRouter = Router();
 
@@ -8,4 +9,16 @@ debugRouter.get("/debug/env-check", (_req, res) => {
     ok: true,
     environment: getEnvPresenceReport()
   });
+});
+
+debugRouter.get("/debug/recent-events", async (_req, res, next) => {
+  try {
+    const events = await getRecentDebugEvents();
+    res.json({
+      ok: true,
+      ...events
+    });
+  } catch (error) {
+    next(error);
+  }
 });
